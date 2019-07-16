c063c57ad96feeaaefb527eed43335edb941ce36
shady smaoui <shadysmaoui@hotmail.fr>
2019-07-16 17:13:44 -0400

    fix

--- flask-api/sections/rest-api-sections/section3/app.py
+++ flask-api/sections/rest-api-sections/section3/app.py
@@ -2,10 +2,16 @@ from flask import Flask,jsonify,request,render_template
 
 app = Flask(__name__)
 
-stores = [{
-    'name': 'My Store',
-    'items': [{'name':'my item', 'price': 15.99 }]
-}]
+stores = [
+    {
+      'name': 'My Store',
+      'items': [
+		    {
+			  'name':'my item', 'price': 15.99 
+			}
+		]
+    }
+]
 
 @app.route('/')
 def home():
